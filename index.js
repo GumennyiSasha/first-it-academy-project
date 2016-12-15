94c86da28af4223c6907e72526165cabd6cb1b32
Olexandr <senbernaro@gmail.com>
2016-12-15 16:13:44 +0200

    14

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-$(function () {
+$(document).ready(function() {
+  var $slider = $('.banners');
+  $slider.slick({
+    arrows:false,
+    dots:true,
+    autoplay:true,
+    autoplayspeed:2000,
+  });
+
+  var $slider2 = $('.posts');
+  $slider2.slick({
+    infinite:true,
+    slidestoshow:4,
+    slidestoscroll:1,
+    autoplay:true,
+    autoplayspeed:2000,
+  });
+});
+
+
+/*$(function () {
   $('.banners').slick({
     arrows:false,
     dots:true,
     autoplay:true,
     autoplayspeed:2000,
   });
-})
+})*/
+
